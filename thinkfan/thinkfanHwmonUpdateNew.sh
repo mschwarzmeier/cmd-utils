2c8c42e8a83598d614468c198a817af3f83f23c1
mschwarzmeier <89848589+mschwarzmeier@users.noreply.github.com>
2025-03-26 13:29:45 +0100

    enable two-digit hwmon-numbers

--- thinkfan/thinkfanHwmonUpdateNew.sh
+++ thinkfan/thinkfanHwmonUpdateNew.sh
@@ -5,7 +5,9 @@ THINKFAN_CONF="/etc/thinkfan.conf"
 
 
 # find the number of hwmon, e.g. "hwmon6" -> 6
-HWMONNUMBER=$(ls /sys/devices/platform/thinkpad_hwmon/hwmon | grep -o '[0-9]')
+HWMONNUMBER=$(ls /sys/devices/platform/thinkpad_hwmon/hwmon | grep -o '[0-9]\+')
 
 # replace with the found HWMONNUMBER
-sed -i "s/\\/hwmon\\/hwmon./\\/hwmon\\/hwmon$HWMONNUMBER/g" "$THINKFAN_CONF"
+sed -i "s|/hwmon/hwmon[0-9]\+|/hwmon/hwmon$HWMONNUMBER|g" "$THINKFAN_CONF"
+
+
